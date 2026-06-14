d0df72d6615b3d69b91292590486b1ca6c563a02
digimind34 <digitalmind34@gmail.com>
2026-06-14 18:53:03 -0400

    Add route table EC2 instance and S3 bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -59,3 +59,41 @@ resource "aws_security_group" "web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_instance" "web_server" {
+  ami                         = "ami-0c02fb55956c7d316"
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public.id
+  vpc_security_group_ids      = [aws_security_group.web_sg.id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "${var.project_name}-ec2"
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "${var.project_name}-public-rt"
+  }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_s3_bucket" "project_bucket" {
+  bucket = "${var.project_name}-bucket-demo-2026"
+
+  tags = {
+    Name = "${var.project_name}-s3"
+  }
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -9,3 +9,15 @@ output "public_subnet_id" {
 output "private_subnet_id" {
   value = aws_subnet.private.id
 }
+
+output "route_table_id" {
+  value = aws_route_table.public_rt.id
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.web_server.public_ip
+}
+
+output "s3_bucket_name" {
+  value = aws_s3_bucket.project_bucket.bucket
+}
